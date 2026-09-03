b8b098074d88620a5263b898c8c151e79f9fcfee
angelicamaputol25-pixel <angelicamaputol25@gmail.com>
2026-09-03 23:33:46 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,13 +7,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+  apiKey: "AIzaSyDgg55CUQ0p196KqhAQW6WtB2VROYvsnSI",
+  authDomain: "rusi-motors-v2.firebaseapp.com",
+  projectId: "rusi-motors-v2",
+  storageBucket: "rusi-motors-v2.firebasestorage.app",
+  messagingSenderId: "399283633846",
+  appId: "1:399283633846:web:e76d3f2f96fbd3adc03017",
+  measurementId: "G-EB3T6SLDS9"
 };
 
 const app  = initializeApp(firebaseConfig);
